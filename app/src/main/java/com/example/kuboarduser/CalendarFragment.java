1d7a494ef1e2964444ca100328ae4cf9a30d0f59
jyh <jeonyh0521@gmail.com>
2020-09-29 16:08:41 +0900

    하단 메뉴바 생성

---
+++ app/src/main/java/com/example/kuboarduser/CalendarFragment.java
@@ -0,0 +1,24 @@
+package com.example.kuboarduser;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CalendarFragment extends Fragment {
+    // 캘린더 화면 뿌려주기
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.calendar_fragment, container, false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/kuboarduser/MainActivity.java
+++ app/src/main/java/com/example/kuboarduser/MainActivity.java
@@ -1,18 +1,77 @@
 package com.example.kuboarduser;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+
+    private ChatFragment chatFragment;
+    private BoardFragment boardFragment;
+    private CalendarFragment calendarFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_board:
+                        setFragment(0);
+                        break;
+                    case R.id.action_chat:
+                        setFragment(1);
+                        break;
+                    case R.id.action_calendar:
+                        setFragment(2);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        chatFragment = new ChatFragment();
+        boardFragment = new BoardFragment();
+        calendarFragment = new CalendarFragment();
+
+        // 첫 화면(게시판) 출력
+        setFragment(0);
+    }
+
+    // Fragment 교체
+    private void setFragment(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame, boardFragment);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, chatFragment);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, calendarFragment);
+                ft.commit();
+                break;
+        }
+
     }
 }
-
-// 전용후
-//혜진짜응.....
-//양효민
